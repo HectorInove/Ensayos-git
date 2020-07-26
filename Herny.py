6e04e0c7f1e424a772d400521c4bbcea89841a20
Hernan Contigiani <65074450+hcontigiani-inove@users.noreply.github.com>
2020-07-26 18:07:37 -0300

    Update Herny.py

--- Herny.py
+++ Herny.py
@@ -3,11 +3,9 @@ print('Hola Hector!')
 a = 5
 b = 6
 
-<<<<<<< HEAD
 # suma = sabes cuanto es??
 
+suma = a + b
 
 print(a+b)
-=======
-suma = a + b
->>>>>>> 5f1a3d60b3390f0099a592bdbd7aeb756fcd4ba8
+
